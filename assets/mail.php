a3ded2903dc980083dfaa25dbc514522c7a040d0
Randy Christenhusz <onederlnd@gmail.com>
2024-08-13 21:44:07 -0400

    08/13/2024 (21:44)

--- assets/mail.php
+++ assets/mail.php
@@ -1,5 +1,5 @@
 <?php 
-if(isset($_POST['submit'])){
+/* if(isset($_POST['submit'])){
     $to = "marla@meltzerdesign.com"; // this is your Email address
     $email = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['first_name'];
@@ -25,4 +25,47 @@ if(isset($_POST['submit'])){
     // You cannot use header and echo together. It's one or the other.
     header('Location: ../thank_you.html');
     }
+    */
+
+
+   /*  This PHP form mailing script created by SiteGrinder 3.6 s_337  http://www.medialab.com/sitegrinder3  */
+
+function stripFormSlashes($arr)
+    {
+ 	    if(!is_array($arr))
+ 	    {
+ 		    return stripslashes($arr);
+ 	    }
+ 	    else
+ 	    {
+ 		    return array_map('stripFormSlashes', $arr);
+ 	    }
+     }
+ 
+     if(get_magic_quotes_gpc())
+     {
+ 	    $_POST = stripFormSlashes($_POST);
+     }
+  $message = "";
+  $message .= "First Name: " . htmlspecialchars($_POST['first_name'], ENT_QUOTES) . "<br \>\n";
+  $message .= "Last Name: " . htmlspecialchars($_POST['last_name'], ENT_QUOTES) . "<br \>\n";
+  $message .= "Email: " . htmlspecialchars($_POST['email'], ENT_QUOTES) . "<br \>\n";
+  $message .= "Phone: " . htmlspecialchars($_POST['telephone'], ENT_QUOTES) . "<br \>\n";
+  $message .= "Company: " . htmlspecialchars($_POST['company'], ENT_QUOTES) . "<br \>\n";
+  $message .= "Comments: " . htmlspecialchars($_POST['message'], ENT_QUOTES) . "<br \>\n";
+  $lowmsg = strtolower($message);
+  $injection_strings = array ( "content-type:","charset=","mime-version:","multipart/mixed","bcc:","cc:");
+  foreach($injection_strings as $suspect)
+  {
+   if((stristr($lowmsg, $suspect)) || (stristr(strtolower($_POST['first_name']), $suspect)) || (stristr(strtolower($_POST['email']), $suspect)))
+   {
+     die ( 'Illegal Input.  Go back and try again.  Your message has not been sent.' );
+   }
+  }
+ $headers = "MIME-Version: 1.0\r\nContent-type: text/html; charset=utf-8\r\n";
+ $headers .= "Content-Transfer-Encoding: 8bit\r\n";
+ $headers .= "From: \"" . $_POST['first_name'] . " " . $_POST['last_name'] . "\" <" . $_POST['email'] . ">\r\n";
+ $headers .= "Reply-To: " . $_POST['email'] . "\r\n";
+  mail("rchristenhusz@brandnutmarketing.com", "From Marla Meltzer Graphic Design dot com", $message, $headers);
+  header("Location: ../thank_you.html");
 ?>
